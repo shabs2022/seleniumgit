5f56c8ad8e762e46c2e0241e6591a9a6dd9ce5d5
anup <anup.b511@gmail.com>
2026-03-04 15:30:49 +0530

    committed the modification

--- src/pack/anup_newclass.java
+++ src/pack/anup_newclass.java
@@ -4,7 +4,7 @@ public class anup_newclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testing");
+		System.out.println("Testing modification changed");
 
 	}
 
